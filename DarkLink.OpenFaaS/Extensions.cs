45bb601fa519a44b06d49b512ee87d038bb4ff47
Waldemar Tomme <waldetomme@gmail.com>
2022-03-06 15:16:59 +0100

    Map every http method

--- DarkLink.OpenFaaS/Extensions.cs
+++ DarkLink.OpenFaaS/Extensions.cs
@@ -58,7 +58,7 @@ public static class Extensions
         });
 
     public static IEndpointConventionBuilder MapFunction(this IEndpointRouteBuilder builder, FunctionContextDelegate function)
-        => builder.MapPost("/", async context =>
+        => builder.Map("/", async context =>
         {
             try
             {
